9aa4e74ac66682e4bf4da2fa54b097e3c4ba6647
anshuman7negi <anshumannegi777@gmail.com>
2023-07-24 17:45:48 +0530

    Added score name and list dynamically

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './style.css';
+import display from './modules/display.js';
+
+const data = [
+  {
+    name: 'anshuman',
+    score: 25,
+  },
+  {
+    name: 'jack',
+    score: 56,
+  },
+  {
+    name: 'ajay',
+    score: 55,
+  },
+  {
+    name: 'manmohan',
+    score: 77,
+  },
+];
+
+display(data);
