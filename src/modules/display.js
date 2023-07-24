88831cba01283338e94f6c2bd375ff4b21c3a413
anshuman7negi <anshumannegi777@gmail.com>
2023-07-24 18:05:29 +0530

    Solved linter errors

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,8 +1,8 @@
 export default (data) => {
-    const scoreList = document.querySelector('.score-list');
-    data.forEach((item) => {
-      const li = document.createElement('li');
-      li.innerHTML = `${item.name} : ${item.score}`;
-      scoreList.appendChild(li);
-    });
-  };
+  const scoreList = document.querySelector('.score-list');
+  data.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${item.name} : ${item.score}`;
+    scoreList.appendChild(li);
+  });
+};
